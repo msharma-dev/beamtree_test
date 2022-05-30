95699b8b7d4e51879ec4ec15147759f7e36dca7a
TD-Mayank <mayank.tiwari@techdilation.com>
2022-05-30 21:44:45 +0530

    completed searc

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,14 @@
 class SearchController < ApplicationController
     def index
-        @people = Person.all
-    end
-
-    def search
-        @people = Person.all
+        @searchtext = params['searchtext']
+        if !@searchtext.blank?
+            @people = Person.all.where(:code=>@searchtext)
+        else
+            @people = Person.all
+        end
+        respond_to do |format|
+            format.html { render action: 'index' }
+            format.json { render json: @people.errors, status: :unprocessable_entity }
+        end
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   resources :events
   resources :people
-  resources :search, only: [:index]
+  resources :search, only: [:index] do
+    collection do
+      get 'index'
+      post 'index'
+    end
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
